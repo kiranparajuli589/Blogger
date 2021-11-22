62cfcd6521eafec0ff202f8713167626d2b2cdc9
anish <anish@anishs-MacBook-Pro.local>
2021-11-22 16:52:40 +0545

    just editing

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -7,25 +7,27 @@ module.exports = {
   test_settings: {
     default: {
       launch_url:`${HOST}:${PORT}`,
-
-        selenium_host: 'localhost',
-        desiredCapabilities: {
-          browserName: 'chrome',
-          javascriptEnabled: true,
-          acceptSslCerts: true,
-          chromeOptions: {
-            args: ['disable-gpu', 'ignore-certificate-errors'],
-            w3c: false
-          },
-          loggingPrefs: {browser: 'ALL'}
+      globals: {
+        user_api_url: 'http://localhost:3001/api/users/',
+      },
+      selenium_host: 'localhost',
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        chromeOptions: {
+          args: ['disable-gpu', 'ignore-certificate-errors'],
+          w3c: false
         },
-        webdriver: {
-          server_path: chromedriver.path,
-          cli_args: ['--port=4444'],
-          start_process: false,
-          port: 4444,
-          use_legacy_jsonwire: false
-        },
+        loggingPrefs: {browser: 'ALL'}
+      },
+      webdriver: {
+        server_path: chromedriver.path,
+        cli_args: ['--port=4444'],
+        start_process: false,
+        port: 4444,
+        use_legacy_jsonwire: false
+      },
 
 
     }
