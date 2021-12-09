70baf2f9dcf916599e98c392938a0b8633d0782f
PKiran <39373750+kiranparajuli589@users.noreply.github.com>
2021-12-09 17:00:17 +0545

    [tests-only] Added hooks raw code (#11)
    * Added hooks raw code
    
    * Use fixtures folder for test data

--- tests/acceptance/stepDefinitions/loginContext.js
+++ tests/acceptance/stepDefinitions/loginContext.js
@@ -1,5 +1,46 @@
-const {Given, When, Then} = require('@cucumber/cucumber')
+const {Given, When, Then, After, Before} = require('@cucumber/cucumber')
 const {client} = require('nightwatch-api')
+const axios = require("axios")
+const util = require("util")
+const {cli} = require("nightwatch")
+const urls = require("tests/acceptance/fixtures/urls.json")
+
+const userDetailUrl = client.launch_url + urls.user.detail
+const userListUrl = client.launch_url + urls.user.list
+
+let createdUsersList = []
+
+After(() => {
+    if (createdUsersList.length > 0) {
+        createdUsersList.forEach((userToDelete, index) => {
+            axios.delete(util.format(userDetailUrl, userToDelete.id))
+            .then(() => {
+                delete createdUsersList.splice(index, 1)
+            })
+        })
+    }
+})
+
+Given("the user has been created with following details:", dataTable => {
+    const userToBeCreated = dataTable.rowsHash()
+    axios.post(userListUrl, userToBeCreated)
+        .then(res => {
+            createdUsersList.push({
+                id: res['id'],
+                username: res['username']
+            })
+        })
+        .catch((err) => {
+            throw new Error("User with username " + dataTable.username + " cannot be created.\nError:\n" + err)
+        })
+
+})
+
+Given("the following users have been created with following details:", dataTable => {
+    const createdUsers = [{id: 3, email: ''}, {id: 4}]
+    createdUsersList = createdUsers
+})
+
 
 Given('the user has navigated to the homepage', function () {
     return client.url(client.launchUrl)
