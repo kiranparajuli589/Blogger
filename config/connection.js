b4313d20aaf18a42ca808eaf76e007e94890d7a1
Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
2024-08-07 13:20:00 +0545

    Added docker compose for db
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>

--- config/connection.js
+++ config/connection.js
@@ -12,9 +12,9 @@ if (process.env.JAWSDB_URL) {
   else console.log("Sorry, cannot connect to JAW DB.")
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: "localhost",
+    host: process.env.DB_HOST,
     dialect: "mysql",
-    port: 3306
+    port: process.env.DB_PORT
   })
   if (!sequelize) console.log("Sorry, cannot connect to the local database.")
   else console.log("Boom! Local DB connected.")
